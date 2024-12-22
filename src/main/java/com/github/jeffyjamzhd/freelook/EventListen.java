fbba5b2f11fa9a12efd2b7a106031d79d0ccff79
Xy_Lose <xylose8126@163.com>
2024-12-22 18:28:01 +0800

    Revised the Chinese translation and removed useless classes

--- src/main/java/com/github/jeffyjamzhd/freelook/EventListen.java
+++ src/main/java/com/github/jeffyjamzhd/freelook/EventListen.java
@@ -21,11 +21,5 @@ public class EventListen {
 
     @Subscribe
     public void handleChatCommand(HandleChatCommandEvent event) {
-        String par2Str = event.getCommand();
-        EntityPlayer player = event.getPlayer();
-        if (par2Str.startsWith("Hello")) {
-            player.sendChatToPlayer(ChatMessageComponent.createFromText("Hello, FML!"));
-            event.setExecuteSuccess(true);
-        }
     }
 }
